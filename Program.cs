06c6645b005a9597336f70777e33c2eb7d3b1430
thiago <e107ti@cps.sp.gov.br>
2022-03-18 20:15:56 -0300

    finalizei o exercicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int n1, n2, R;
+
+Console.WriteLine("calculo da soma entre dois numeros./n");
+
+Console.Write("Digite o primeiro numero: ");
+n1 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Digite o segundo numero: ");
+n2 = Convert.ToInt32(Console.ReadLine()!);
+R = n1 + n2;
+
+Console.WriteLine($"soma: {R}");
